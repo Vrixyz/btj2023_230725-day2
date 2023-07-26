9f2e9a3d8937dd3c940ab203f3a2196957b82f89
Thierry Berger <contact@thierryberger.com>
2023-07-26 11:07:32 +0200

    more arcade-y jump than montgolfiere

---
+++ src/ground_detection.rs
@@ -0,0 +1,8 @@
+/*use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
+fn update_ground_state(
+    rapier_context: Res<RapierContext>,
+    ghost_platforms_query: Query<Transform, With<Player>>,
+) {
+}*/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod ground_detection;
+
 use bevy::{
     app::AppExit,
     math::{vec2, vec3},
@@ -5,7 +7,7 @@ use bevy::{
     render::camera::ScalingMode,
     sprite::MaterialMesh2dBundle,
 };
-use bevy_rapier2d::prelude::*;
+use bevy_rapier2d::{na::ComplexField, prelude::*};
 use bevy_rapier_collider_gen::*;
 use std::f32::consts::*;
 
@@ -50,7 +52,9 @@ struct MapImageHandle {
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
+    mut rapier_configuration: ResMut<RapierConfiguration>,
 ) {
+    rapier_configuration.gravity = Vect::Y * (-9.81 * 10.0 * 10.0);
     commands.spawn(Camera2dBundle {
         projection: OrthographicProjection {
             scaling_mode: ScalingMode::AutoMin {
@@ -68,35 +72,44 @@ fn setup(
     });
 }
 
+#[derive(Component)]
+pub struct TargetAngVel(pub Option<f32>);
+
 fn arrow_keys_apply_force(
-    mut commands: Commands,
     keyboard_input: Res<Input<KeyCode>>,
-    q_player: Query<Entity, With<Player>>,
+    mut q_player: Query<(&mut Velocity, &mut TargetAngVel), With<Player>>,
     time: Res<Time>,
 ) {
-    for entity in q_player.iter() {
+    for (mut vel, mut target_angvel) in q_player.iter_mut() {
         let mut impulse = vec2(0.0, 0.0);
-        let mut torque_impulse = 0.0;
-
-        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-            torque_impulse += time.delta_seconds() * 0.02;
+        let mut initial_target_vel = if let Some(target_angvel) = target_angvel.0 {
+            target_angvel
+        } else {
+            vel.angvel
+        };
+        let mut newTargetAngVel = initial_target_vel;
+        let mut explicit_angvel = false;
+        if keyboard_input.any_pressed([KeyCode::Left, KeyCode::A]) {
+            newTargetAngVel += time.delta_seconds() * 8f32;
+            explicit_angvel = true;
         }
-        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-            torque_impulse -= time.delta_seconds() * 0.02;
+        if keyboard_input.any_pressed([KeyCode::Right, KeyCode::D]) {
+            newTargetAngVel -= time.delta_seconds() * 8f32;
+            explicit_angvel = true;
+        }
+        if !explicit_angvel {
+            target_angvel.0 = None;
+        } else {
+            if newTargetAngVel.abs() < initial_target_vel.abs() {
+                newTargetAngVel *= 0.8f32;
+            }
+            newTargetAngVel = newTargetAngVel.clamp(-15f32, 15f32);
+            target_angvel.0 = Some(newTargetAngVel);
+            vel.angvel = dbg!(newTargetAngVel);
         }
 
-        if keyboard_input.pressed(KeyCode::Space)
-            || keyboard_input.pressed(KeyCode::W)
-            || keyboard_input.pressed(KeyCode::Up)
-        {
-            impulse = Vec2::new(0.0, time.delta_seconds() * 10.0);
-        }
-
-        if impulse != Vec2::ZERO || torque_impulse != 0.0 {
-            commands.entity(entity).insert(ExternalImpulse {
-                impulse: impulse,
-                torque_impulse: torque_impulse,
-            });
+        if keyboard_input.any_just_pressed([KeyCode::Space, KeyCode::W, KeyCode::Up]) {
+            vel.linvel = Vect::new(vel.linvel.x, 360f32);
         }
     }
 }
@@ -127,11 +140,16 @@ fn generate_map_collider(
 
             // spawn player
             commands
-                .spawn(Player)
-                .insert(RigidBody::Dynamic)
+                .spawn((Player, TargetAngVel(None)))
+                .insert((
+                    RigidBody::Dynamic,
+                    Velocity::default(),
+                    Ccd { enabled: true },
+                ))
                 .insert(Collider::ball(15.0))
                 .insert(Restitution::coefficient(0.7))
                 .insert(Friction::new(5.0))
+                //.insert(AdditionalMassProperties::Mass(0.01f32))
                 .insert(SpriteBundle {
                     texture: asset_server.load("player.png"),
                     transform: Transform::from_xyz(0.0, 20.0, 1.0),
